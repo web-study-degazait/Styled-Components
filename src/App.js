7cab897bf2e99c1aef7a488ceb2372754b663046
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-10-01 22:40:39 +0900

    dark white mode

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 36px;
-    cursor: pointer;
-    &:hover {
-      font-size: 40px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <span>:D</span>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
-
 export default App;
